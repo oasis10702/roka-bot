2217ba00ba5253fe3dfdb5446ce916d408269a24
Hiro <hiro_ho@htc.com>
2019-09-20 02:03:57 +0800

    update

--- src/replier.js
+++ src/replier.js
@@ -1,9 +1,11 @@
 const MessageReply = (msg, text) => msg.reply(text);
 
+const MessageChannelSend = (msg, text) => msg.channel.send(text);
+
 export const HugReplier = message => {
     MessageReply(message, '囉卡醬要抱抱！');
 };
 
 export const ConfuseReplier = message => {
-    MessageReply(message, '囉卡聽不懂喵！');
+    MessageChannelSend(message, '囉卡聽不懂喵！');
 }
